767be6ee2f07785bc1637af5e530fd351b6eee81
arakiriki <szn0125@gmail.com>
2023-01-09 18:38:50 +0900

    postのCRUDとlike

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,84 @@
 const router = require("express").Router();
+const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("posts router");
+//投稿を作成する
+router.post("/", async (req, res) =>{
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    return res.status(200).json(savedPost);
+  }catch(err){
+    return res.status(500).json(err);
+  }
+
 });
+
+//投稿を更新する
+router.put("/:id", async (req, res) =>{
+  try {
+    const post = await Post.findById(req.params.id);
+    if(post.userId === req.body.userId){
+      await post.updateOne({
+        $set: req.body,
+      });
+      return res.status(200).json("投稿の編集が完了しました。")
+    }else {
+      return res.status(403).json("あなたは他の人の投稿を編集できません。");
+    }
+  }catch(err){
+    return res.status(403).json(err);
+  }
+});
+
+//投稿を削除する
+router.delete("/:id", async (req, res) =>{
+  try {
+    const post = await Post.findById(req.params.id);
+    if(post.userId === req.body.userId){
+      await post.deleteOne();
+      return res.status(200).json("投稿の削除が完了しました。")
+    }else {
+      return res.status(403).json("あなたは他の人の投稿を削除できません。");
+    }
+  }catch(err){
+    return res.status(403).json(err);
+  }
+});
+
+//投稿を取得する
+router.get("/:id", async (req, res) =>{
+  try {
+    const post = await Post.findById(req.params.id);
+    return res.status(200).json(post);
+  }catch(err){
+    return res.status(403).json(err);
+  }
+});
+
+//特定の投稿に「like」を押す
+router.put("/:id/like", async (req, res) =>{
+    try{
+      const post = await Post.findById(req.params.id);
+      //投稿に「like」されていないことを確認する
+      if(!post.likes.includes(req.body.userId)) {
+        await post.updateOne({
+          $push: {
+            likes: req.body.userId,
+          },
+        });
+        return res.status(200).json("投稿に「like」をしました。");
+      }else{
+        await post.updateOne({
+          $pull: {
+            likes: req.body.userId,
+          } 
+        });
+        return res.status(200).json("投稿の「like」を解除しました。");
+      }
+    }catch(err){
+      return res.status(500).json(err);
+    }
+})
+
 
 module.exports = router;
