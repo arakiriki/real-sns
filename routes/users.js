fcf2751048be8ed58bb656cb9c3238cc0cff73ec
arakiriki <szn0125@gmail.com>
2023-01-09 17:36:36 +0900

    3-23フォロー機能の実装

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,87 @@
 const router = require("express").Router();
+const User = require("../models/User");
+
+//CRUD
+//ユーザー情報の更新
+router.put("/:id", async(req, res) => {
+
+  if( req.body.userId === req.params.id || req.body.isAdmin) {
+    try{
+      const user = await User.findByIdAndUpdate(req.params.id,{
+        $set: req.body,
+      });
+      res.status(200).json("ユーザー情報が更新されました。"); 
+    }catch (err){
+      return res.status(500).json(err);
+    }
+  }else{
+    return res.status(403)
+    .json("あなたは自分のアカウントのみ情報更新可能です。")
+  }
+});
+
+//ユーザー情報の削除
+router.delete("/:id", async(req, res) => {
+
+  if( req.body.userId === req.params.id || req.body.isAdmin) {
+    try{
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("ユーザー情報が削除されました。"); 
+    }catch (err){
+      return res.status(500).json(err);
+    }
+  }else{
+    return res.status(403)
+    .json("あなたは自分のアカウントのみ削除可能です。")
+  }
+});
+
+//ユーザー情報の取得
+router.get("/:id", async(req, res) => {
+    try{
+      const user = await User.findById(req.params.id);
+      const { password, updatedAt, ...other} = user._doc;
+      res.status(200).json(other); 
+    }catch (err){
+      return res.status(500).json(err);
+    }
+});
+
+
+//ユーザーのフォロー
+router.put("/:id/follow", async (req, res) =>{
+  if(req.body.userId !== req.params.id){
+    try{
+
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      //フォロワーに自分が含まれていないことを確認する
+      if(!user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $push: {
+            followers: req.body.userId,
+          },
+        });
+
+        await currentUser.updateOne({
+          $push: {
+            followings: req.params.id,
+          },
+        });
+
+        return res.status(200).json("フォローに成功しました。");
+      }else{
+        return res.status(403).json("あなたはすでにこのユーザーをフォローしています。");
+      }
+
+    }catch(err){
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(500).json("自分自身のフォローはできません。");
+  }
+})
 
 // router.get("/", (req, res) => {
 //   res.send("posts router");
